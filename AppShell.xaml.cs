663742048918ab2e060d9d1a97291a19c98a6b5f
javi <javi@DESKTOP-50SMRNB>
2025-05-10 12:21:55 +0200

    config android no funcional

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -8,9 +8,17 @@ namespace LaCasaDelSueloRadianteApp
         {
             InitializeComponent();
 
-            // Registrar la ruta para AgregarPage
+            // Registrar las rutas de las páginas
             Routing.RegisterRoute(nameof(AgregarPage), typeof(AgregarPage));
+            Routing.RegisterRoute(nameof(ClientesPage), typeof(ClientesPage));
             Routing.RegisterRoute(nameof(FullScreenImagePage), typeof(FullScreenImagePage));
+            Routing.RegisterRoute(nameof(EditarClientePage), typeof(EditarClientePage));
+            Routing.RegisterRoute(nameof(HistorialPage), typeof(HistorialPage));
+            Routing.RegisterRoute(nameof(InformacionPage), typeof(InformacionPage));
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(ServicioDetallePage), typeof(ServicioDetallePage));
+            Routing.RegisterRoute(nameof(ServiciosPage), typeof(ServiciosPage));
         }
     }
 }
--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,16 +1,16 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Microsoft.Maui;
 
-namespace LaCasaDelSueloRadianteApp
+namespace com.companyname.lacasadelsueloradianteapp;
+
+[Application]
+public class MainApplication : MauiApplication
 {
-    [Application]
-    public class MainApplication : MauiApplication
+    public MainApplication(nint handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
     {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
-        {
-        }
-
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
