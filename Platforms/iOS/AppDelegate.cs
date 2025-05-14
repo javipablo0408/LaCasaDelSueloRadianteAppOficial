d6da4adeae33921618a5698b92c6789537f145c3
javi <javi@DESKTOP-50SMRNB>
2025-05-14 20:12:10 +0200

    funciona todo menos ver los datos en android

--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
 ﻿using Foundation;
+using Microsoft.Identity.Client; // Necesario para MSAL
+using UIKit;
 
 namespace LaCasaDelSueloRadianteApp
 {
@@ -6,5 +8,12 @@ namespace LaCasaDelSueloRadianteApp
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        // Manejo de la redirección de autenticación MSAL
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(url);
+            return base.OpenUrl(app, url, options);
+        }
     }
 }
